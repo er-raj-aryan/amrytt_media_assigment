b4d5d3fec74f827c3b3bba333f33781dd11b7870
Raj Aryan <raj267447@gmail.com>
2024-12-16 14:53:12 +0530

    deploy server

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const password = process.env.MONGODB_PASSWORD ? process.env.MONGODB_PASSWORD.tri
 // middleware
 app.use(express.json());
 app.use( cors({
-    origin: ["http://localhost:8000"],
+    origin: ["http://localhost:8000", "https://amrytt-media-assgiment-client.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }));
